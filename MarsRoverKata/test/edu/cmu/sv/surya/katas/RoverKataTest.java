7105afcf066b0cbb6edfa0d00cc33adb8e5229a1
Surya Kiran <suryakiranl@gmail.com>
2014-03-20 00:37:19 -0700

    Update test cases to pass all

--- MarsRoverKata/test/edu/cmu/sv/surya/katas/RoverKataTest.java
+++ MarsRoverKata/test/edu/cmu/sv/surya/katas/RoverKataTest.java
@@ -2,128 +2,139 @@ package edu.cmu.sv.surya.katas;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import edu.cmu.sv.surya.katas.Grid.Direction;
 
 public class RoverKataTest {
-	Rover rover;
+	Grid grid;
 	@Before
 	public void beforeEachMethod() {
-		GridSize size = new GridSize(100,100);
-		Position initialPosition = new Position(0, 0,size);
-		Direction initialDirection = Direction.EAST;
+		GridSize gridSize = new GridSize(100,100);
+		grid = new Grid(gridSize);
 		
-		rover = new Rover(initialPosition, initialDirection);
+		Set<Position> obstacles = new HashSet<Position>();
+		obstacles.add(new Position(1, 1));
+		obstacles.add(new Position(10, 18));
+		obstacles.add(new Position(9, 11));
+		obstacles.add(new Position(11, 7));
+		obstacles.add(new Position(50, 50));
+		obstacles.add(new Position(93, 3));
+		grid.setObstacles(obstacles);
+		
+		Position initialPosition = new Position(0, 0);
+		Direction initialDirection = Direction.EAST;
+		grid.placeRoverOnGrid(initialPosition, initialDirection);
+
 	}
 	
 	@Test
 	public void test1() {
 		String navigation = "f";
-		RoverKata.traverse(rover, navigation);
-		assertEquals(rover.getCurrentPosition().getXPos(), 0);
-		assertEquals(rover.getCurrentPosition().getYPos(), 1);
-		assertEquals(rover.getDirection(), Direction.EAST);
+		grid.traverse(navigation);
+		assertEquals(grid.getRover().getPosition().getXPos(), 0);
+		assertEquals(grid.getRover().getPosition().getYPos(), 1);
+		assertEquals(grid.getRover().getDirection(), Direction.EAST);
 	}
 	
 	@Test
 	public void test2() {
 		String navigation = "ff";
-		RoverKata.traverse(rover, navigation);
-		assertEquals(rover.getCurrentPosition().getXPos(), 0);
-		assertEquals(rover.getCurrentPosition().getYPos(), 2);
-		assertEquals(rover.getDirection(), Direction.EAST);
+		grid.traverse(navigation);
+		assertEquals(grid.getRover().getPosition().getXPos(), 0);
+		assertEquals(grid.getRover().getPosition().getYPos(), 2);
+		assertEquals(grid.getRover().getDirection(), Direction.EAST);
 	}
 	
 	@Test
 	public void test3() {
 		String navigation = "b";
-		RoverKata.traverse(rover, navigation);
-		assertEquals(rover.getCurrentPosition().getXPos(), 0);
-		assertEquals(rover.getCurrentPosition().getYPos(), 99);
-		assertEquals(rover.getDirection(), Direction.EAST);
+		grid.traverse(navigation);
+		assertEquals(grid.getRover().getPosition().getXPos(), 0);
+		assertEquals(grid.getRover().getPosition().getYPos(), 99);
+		assertEquals(grid.getRover().getDirection(), Direction.EAST);
 	}
 	
 	@Test
 	public void test4() {
 		String navigation = "bb";
-		RoverKata.traverse(rover, navigation);
-		assertEquals(rover.getCurrentPosition().getXPos(), 0);
-		assertEquals(rover.getCurrentPosition().getYPos(), 98);
-		assertEquals(rover.getDirection(), Direction.EAST);
+		grid.traverse(navigation);
+		assertEquals(grid.getRover().getPosition().getXPos(), 0);
+		assertEquals(grid.getRover().getPosition().getYPos(), 98);
+		assertEquals(grid.getRover().getDirection(), Direction.EAST);
 	}
 	
 	@Test
 	public void test5() {
 		String navigation = "fb";
-		RoverKata.traverse(rover, navigation);
-		assertEquals(rover.getCurrentPosition().getXPos(), 0);
-		assertEquals(rover.getCurrentPosition().getYPos(), 0);
-		assertEquals(rover.getDirection(), Direction.EAST);
+		grid.traverse(navigation);
+		assertEquals(grid.getRover().getPosition().getXPos(), 0);
+		assertEquals(grid.getRover().getPosition().getYPos(), 0);
+		assertEquals(grid.getRover().getDirection(), Direction.EAST);
 	}
 	
 	@Test
 	public void test6() {
 		String navigation = "r";
-		RoverKata.traverse(rover, navigation);
-		assertEquals(rover.getCurrentPosition().getXPos(), 0);
-		assertEquals(rover.getCurrentPosition().getYPos(), 0);
-		assertEquals(rover.getDirection(), Direction.SOUTH);
+		grid.traverse(navigation);
+		assertEquals(grid.getRover().getPosition().getXPos(), 0);
+		assertEquals(grid.getRover().getPosition().getYPos(), 0);
+		assertEquals(grid.getRover().getDirection(), Direction.SOUTH);
 	}
 	
 	@Test
 	public void test7() {
 		String navigation = "rr";
-		RoverKata.traverse(rover, navigation);
-		assertEquals(rover.getCurrentPosition().getXPos(), 0);
-		assertEquals(rover.getCurrentPosition().getYPos(), 0);
-		assertEquals(rover.getDirection(), Direction.WEST);
+		grid.traverse(navigation);
+		assertEquals(grid.getRover().getPosition().getXPos(), 0);
+		assertEquals(grid.getRover().getPosition().getYPos(), 0);
+		assertEquals(grid.getRover().getDirection(), Direction.WEST);
 	}
 	
 	@Test
 	public void test8() {
 		String navigation = "l";
-		RoverKata.traverse(rover, navigation);
-		assertEquals(rover.getCurrentPosition().getXPos(), 0);
-		assertEquals(rover.getCurrentPosition().getYPos(), 0);
-		assertEquals(rover.getDirection(), Direction.NORTH);
+		grid.traverse(navigation);
+		assertEquals(grid.getRover().getPosition().getXPos(), 0);
+		assertEquals(grid.getRover().getPosition().getYPos(), 0);
+		assertEquals(grid.getRover().getDirection(), Direction.NORTH);
 	}
 	
 	@Test
 	public void test9() {
 		String navigation = "ll";
-		RoverKata.traverse(rover, navigation);
-		assertEquals(rover.getCurrentPosition().getXPos(), 0);
-		assertEquals(rover.getCurrentPosition().getYPos(), 0);
-		assertEquals(rover.getDirection(), Direction.WEST);
+		grid.traverse(navigation);
+		assertEquals(grid.getRover().getPosition().getXPos(), 0);
+		assertEquals(grid.getRover().getPosition().getYPos(), 0);
+		assertEquals(grid.getRover().getDirection(), Direction.WEST);
 	}
 	
 	@Test
 	public void test10() {
 		String navigation = "lr";
-		RoverKata.traverse(rover, navigation);
-		assertEquals(rover.getCurrentPosition().getXPos(), 0);
-		assertEquals(rover.getCurrentPosition().getYPos(), 0);
-		assertEquals(rover.getDirection(), Direction.EAST);
+		grid.traverse(navigation);
+		assertEquals(grid.getRover().getPosition().getXPos(), 0);
+		assertEquals(grid.getRover().getPosition().getYPos(), 0);
+		assertEquals(grid.getRover().getDirection(), Direction.EAST);
 	}
 	
-	@Test
+	@Test(expected=RuntimeException.class)
 	public void test11() {
 		String navigation = "frflblf";
-		RoverKata.traverse(rover, navigation);
-		assertEquals(rover.getCurrentPosition().getXPos(), 0);
-		assertEquals(rover.getCurrentPosition().getYPos(), 0);
-		assertEquals(rover.getDirection(), Direction.NORTH);
+		grid.traverse(navigation);
 	}
 	
 	@Test
 	public void test12() {
 		String navigation = "ffrff";
-		RoverKata.traverse(rover, navigation);
-		assertEquals(rover.getCurrentPosition().getXPos(), 2);
-		assertEquals(rover.getCurrentPosition().getYPos(), 2);
-		assertEquals(rover.getDirection(), Direction.SOUTH);
+		grid.traverse(navigation);
+		assertEquals(grid.getRover().getPosition().getXPos(), 2);
+		assertEquals(grid.getRover().getPosition().getYPos(), 2);
+		assertEquals(grid.getRover().getDirection(), Direction.SOUTH);
 	}
 
 }
